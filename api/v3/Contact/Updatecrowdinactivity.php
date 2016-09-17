121c9c21fa6c7e13363987b422483e7604cd3fe6
Alain Schaefer <advocate@kadeutsch.org>
2016-09-17 08:04:59 +0200

    implemented update for UT Backlog Activity Data

--- api/v3/Contact/Updatecrowdinactivity.php
+++ api/v3/Contact/Updatecrowdinactivity.php
@@ -110,6 +110,96 @@ function updateCrowdinActivityStat($conn) {
 	}	
 }
 
+// Update the Custom Fields in CiviCRM
+function updateUTActivityStat($conn, $log) {
+
+	$bl = array(
+		"backlogURL" => 'https://docs.google.com/spreadsheets/d/1-1rM_MyfxXuX7aT1k8D4HLQ_IwFgMDGXIbCH-3zUIMQ/pub?gid=1144837871&single=true&output=csv',  // this is the link to the backlog tab in UT Backlog
+		"teamURL" => 'https://docs.google.com/spreadsheets/d/1-1rM_MyfxXuX7aT1k8D4HLQ_IwFgMDGXIbCH-3zUIMQ/pub?gid=52742320&single=true&output=csv',
+		"fields" => array( "contact" => 0, "nickname" => 3, "assignedNickname" => 8, "status" => 12, "reviewNickname" => 10, "reviewStatus" => 12, "date" => 9, "duration" => 6,
+					"assignedStatus" => array( "Zugewiesen", "in Arbeit", "Zurueckgewiesen"),
+					"assignedStr" => '<li>Bitte übersetze den Untertitel : <a href="{URL}">%(5)s, Dauer : %(6)s</a></li>',
+					"editURL" => function($data) {
+						return "https://www.youtube.com/timedtext_video?v=" . $data[4];
+					},
+					"reviewStatus" => array("Uebersetzt"),
+					"reviewStr" => '<li>%(8)s wartet seit dem %(9)s auf dein Review von <a href="{URL}">%(5)s</a></li>',
+					"reviewURL" => function($data) {
+						return 'https://docs.google.com/forms/d/1lxKLZinyitDQb-tSGmRNWGtsPDQG2iGXZRlyuvcxrLA/viewform?entry.675763289='.$data[5].'&entry.643804832='.$data[4].'&entry.1356226264='.$data[10].'&entry.1511908839='.$data[8];
+					},
+					),
+	);
+
+	fwrite($log, "loading CSV Array from Google Spreadsheet\n\n");
+	//load the Field Configuration
+	$cfg = $bl["fields"];
+	
+	// load date from Web
+	$backlog = csvToArray($bl["backlogURL"], ',');
+	$team = csvToArray($bl["teamURL"], ',');
+	
+	//Use first row for names
+	$backLogLabels = array_shift($backlog);
+	$teamLabels = array_shift($team);
+	
+	foreach ($team as &$contact) {
+		// Crowdin Contact ID
+		$cid = $contact[ $cfg["contact"]];
+		$nickname = $contact[ $cfg["nickname"] ];		
+		fwrite($log, "doing " . $nickname . "\n");
+		
+		$num30 = 0;
+		$num90 = 0;
+		$dur30 = 0;
+		$dur90 = 0;
+		
+		foreach($backlog as $video) {
+			
+			// Select all Videos where nickname matches
+			if ( $video[ $cfg["assignedNickname"]] == $nickname && strlen($video[ $cfg["date"] ]) > 0 ) {
+
+				// Turn the Date into a Time Variable				
+				$now = new DateTime("now");
+				$vdate = new DateTime($video[ $cfg["date"] ]);
+				$diff = $now->diff($vdate);
+				
+				//calculate duration of video
+				$dur = split(":",$video[ $cfg["duration"] ]);
+				$duration = 0;
+				if ( count($dur) >=3 ) {
+					$duration = $dur[1]*60 + $dur[2];
+				}
+				
+				if ( $diff->days <= 30 ) {
+					$num30 = $num30 + 1;
+					$dur30 = $dur30 + $duration;
+					
+				}
+				
+				if ( $diff->days <= 90 ) {
+					$num90 = $num90 + 1;
+					$dur90 = $dur90 + $duration;
+				}
+			}
+		}
+			
+			
+		// SQL Statement für die Aktualsierung
+		
+		$dur30s = sprintf('%02d:%02d:%02d', ($dur30/3600),($dur30/60%60), $dur30%60);
+		$dur90s = sprintf('%02d:%02d:%02d', ($dur90/3600),($dur90/60%60), $dur90%60);
+				fwrite($log, "$nickname has done $num30 / $dur30s and $num90 / $dur90s \n" );
+
+		$sql = "UPDATE civicrm_value_crowdin_5 SET ut_activity_30_days_59=$num30, ut_activity_3_month_60=$num90, ut_duration_30_days_61='$dur30s', ut_duration_3_month_62='$dur90s' where entity_id = '$cid'";
+		$result1 = $conn->query($sql);			
+			
+		#$sql = "UPDATE civicrm_value_crowdin_5 SET ut_activity_3_month_60=$num90 where entity_id = '$cid'";
+		#$result1 = $conn->query($sql);		
+		
+	}
+	
+}
+
 /**
  * Contact.Updatecrowdinactivity API
  *
@@ -137,7 +227,8 @@ function civicrm_api3_contact_Updatecrowdinactivity($params) {
 	$date_to="";
 	$date_to = loadCrowdinActivity($conn, $date_to); 
 	updateCrowdinActivityStat($conn);
-	
+	updateUTActivityStat($conn, $crowdinLog);
+		
 	fwrite($crowdinLog, "Crowdin ActivityLog finished\n\n");
 	fclose($crowdinLog);
 	
